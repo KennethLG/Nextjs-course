5f043ed2abf032a3fdcde25316c4071e48d83358
KennethLG <orsonleiziaga@gmail.com>
2022-11-07 16:41:01 -0500

    Nested routes

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,8 +1,3 @@
-export default function Post({ params }) {
-    const { id } = params;
-
-    
-    return (
-        <h1>This is the post</h1>
-    )
+export default function PostsPage() {
+    return null
 }
